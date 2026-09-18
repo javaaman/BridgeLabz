c625b3846ba1e025b691bbec5dd6d2ce9eb7173c
Aman Mishra <amanmishra704@gmail.com>
2026-09-18 21:40:25 +0530

    Ability to Calculate Daily Employee Wage based on part time or full time work

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,12 +3,52 @@
    input: process.stdin,
    output: process.stdout
  });
-  rl.question('Enter Employee Name : ', (employeeName) => {
+ 
     const Present = 1;
-    let attendence = Math.floor(Math.random() * 2);
-    if (attendence === Present) console.log(`The ${employeeName} is present.`);
-     else console.log(`The ${employeeName} is not present.`);
+    const Absent = 0;
+    const No_Work = 0;
+    const Part_Time =4;
+    const Full_Time = 8;
+    const wagePerHour = 20;
+ rl.question('Enter Employee Name : ', (employeeName) => {
+
+
+    const  attendence = Math.floor(Math.random() * 2);
+
+      if(attendence === Absent) {
+        console.log(`The ${employeeName} is not present.`);
+        console.log(`Employee Daily Wage is : 0`);
+        rl.close();
+        return;
+      }
+
+    console.log(`The ${employeeName} is present.`);
+
+    const empoyeeType = Math.floor(Math.random() * 3);
+    let workingHours = 0;
+
+
+    switch (empoyeeType) {
+      case No_Work:
+          workingHours = 0;
+          break;
+      case Part_Time:
+          workingHours = 4;
+          break;
+
+      case Full_Time:
+          workingHours = 8;
+          break;
+    }
+
+    const dailyWage = workingHours * wagePerHour;
+    console.log(`Employee Working Hours is : ${workingHours}`); 
+    console.log(`Employee Daily Wage is : ${dailyWage}`);
 
   rl.close();
   }); 
+
+
+ 
+
 
